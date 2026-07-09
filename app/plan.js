38f339af2900305ef529b23d54cc6869905a09c6
Auto Commit <devnull@example.com>
2026-07-09 22:21:23 +0530

    chore: remove unused scripts inside app directory and move testcases

--- app/plan.js
+++
@@ -1,2 +0,0 @@
-const d = new Date(new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" }));
-console.log(`${d.getFullYear()}-${String(d.getMonth() + 1).padStart(2, '0')}-${String(d.getDate()).padStart(2, '0')}`);
